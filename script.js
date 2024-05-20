40ab9999c1c3f776cb75cd0e41e89644a760d155
Douglas Phelipe <douglasphelipedpss@gmail.com>
2024-05-20 02:45:42 -0300

    feito TODO'S: im de jogo e queda do bonec. Fase de TODO para TESTE. Ainda falta apenas um TODO: mover o boneco para esquerda e direita + mudar a posição do boneco de acordo com o botão clicado (esquerda ou direita).

--- script.js
+++ script.js
@@ -1,10 +1,58 @@
 const grade = document.querySelector(".grade");
+const score = document.querySelector(".score");
+let quantidadeDeScore = document.querySelector(".score > span");
 const quantidadeDePlataformas = 5;
 var  plataformasEmJogo = [];
 
-//TODO queda do boneco
-//TODO  mover o boneco para esquerda e direita + mudar a posição do boneco de acordo com o botão (esquerda ou direita) clicado
-//TODO fim de jogo e vitória de jogo
+//TODO  mover o boneco para esquerda e direita + mudar a posição do boneco de acordo com o botão clicado (esquerda ou direita)
+
+// TODO fim de jogo
+// FASE PARA TESTES
+function gameOver() {
+    quantidadeDeScore.remove();
+    score.innerHTML = "Game Over!";
+}
+
+// TODO queda do boneco
+// FASE PARA TESTES
+function cairBoneco() {
+    const boneco = document.querySelector(".boneco");
+
+    // cair boneco
+    const intervalo = setInterval(() => {
+        boneco.style.bottom = Number.parseFloat((boneco.style.bottom).replace(/[a-zA-Z]/g, '')) - 5 + "px";
+        verificarElementos();
+    }, 20);
+
+    // verificar se a cada subida do boneco existe uma plataforma para pular
+    function verificarElementos() {
+        const boneco = document.querySelector(".boneco");
+        const quantidadeDePixelsDoBottomDoBoneco = Number.parseFloat((boneco.style.bottom).replace(/[a-zA-Z]/g, ''));
+
+        plataformasEmJogo.forEach(function (elementoAtual, indice) {
+
+            const pixelsLeftElementoAtual = Number.parseFloat((elementoAtual.style.left).replace(/[a-zA-Z]/g, ''));
+            const pixelsLeftBoneco = Number.parseFloat((boneco.style.left).replace(/[a-zA-Z]/g, ''));
+            const quantidadeDePixelsDoBottomDoElementoAtual = Number.parseFloat((elementoAtual.style.bottom).replace(/[a-zA-Z]/g, ''));
+            
+            // Se o boneco bater nas extremidades ou em cima da plataforma, então, pule
+            if (pixelsLeftBoneco <= pixelsLeftElementoAtual + 75 && ((pixelsLeftBoneco - 10 <= pixelsLeftElementoAtual + 75 &&
+                pixelsLeftBoneco - 10 >= pixelsLeftElementoAtual) || (pixelsLeftBoneco + 51 >= pixelsLeftElementoAtual &&
+                pixelsLeftBoneco + 51 <= pixelsLeftElementoAtual + 75)) && quantidadeDePixelsDoBottomDoBoneco ===
+                quantidadeDePixelsDoBottomDoElementoAtual) {
+                clearInterval(intervalo);
+                moverPlataforma();
+                quantidadeDeScore.innerHTML = Number(quantidadeDeScore.innerHTML) + 1;
+            }
+            
+            // se o boneco chegar ao final da tela, então gameOver
+            if (quantidadeDePixelsDoBottomDoBoneco == 0) {
+                clearInterval(intervalo);
+                gameOver();
+            }
+        })
+    }
+}
 
 function pularBoneco(indice) {
     boneco = document.querySelector(".boneco");
@@ -12,18 +60,27 @@ function pularBoneco(indice) {
     let limiteDePixelsPorPulo = Number.parseFloat((plataformasEmJogo[indice].style.bottom).replace(/[a-zA-Z]/g, ''));
 
     const intervalo = setInterval(() => {
+        pararIntervalo();
         quantidadeDePixels = quantidadeDePixels + 5;
         boneco.style.bottom = quantidadeDePixels + "px";
-        pararIntervalo();
-    }, 10);
+    }, 20);
 
     function pararIntervalo() {
-        if (quantidadeDePixels === limiteDePixelsPorPulo + 190) {
+        // se o boneco chegar ao topo, então desça
+        if (quantidadeDePixels === 520) {
+            console.log("jdjaf")
             clearInterval(intervalo);
+            cairBoneco();
+        }
+        // se alcançar a quantidade de pulos, então desça
+        else if (quantidadeDePixels === limiteDePixelsPorPulo + 200) {
+            clearInterval(intervalo);
+            cairBoneco();
         }
     };
 };
 
+// removidor de plataforma
 function removerPlataforma(indice) {
     plataformasEmJogo[indice].remove();
     plataformasEmJogo.splice(indice, 1);
@@ -34,15 +91,21 @@ function removerPlataforma(indice) {
     pularBoneco(indice);
 };
 
-function moverPlataforma() {
-    let intervalo = setTimeout(() => {
-        plataformasEmJogo.forEach(function (elementoAtual, indice) {
-            let quantidadeDePixels = (elementoAtual.style.bottom).replace(/[a-zA-Z]/g, '');
-            elementoAtual.style.bottom = quantidadeDePixels - 120 + "px";
-            removerPlataformaNoFinalDaGrade(elementoAtual, indice);
-        });
-    }, 10);
 
+function moverPlataforma() {
+    descerPlataformas();
+
+    function descerPlataformas() {
+        let intervalo = setTimeout(() => {
+            plataformasEmJogo.forEach(function (elementoAtual, indice) {
+                let quantidadeDePixels = (elementoAtual.style.bottom).replace(/[a-zA-Z]/g, '');
+                elementoAtual.style.bottom = quantidadeDePixels - 120 + "px";
+                removerPlataformaNoFinalDaGrade(elementoAtual, indice);
+            });
+        }, 100);
+    };
+
+    //remover plataformas que estejam no final da grade
     function removerPlataformaNoFinalDaGrade(elementoAtual, indice) {
             let quantidadeDePixels = Number.parseFloat((elementoAtual.style.bottom).replace(/[a-zA-Z]/g, ''));
         if (quantidadeDePixels <= 0) {
@@ -50,10 +113,12 @@ function moverPlataforma() {
                 removerPlataforma(indice);
             }, 0);
             }
-    }
+    };
 };
 
+// criar plataformas no início e durante o jogo
 function criarPlataforma() {
+    //objeto para criar 5 plataformas e, para o objeto manter sempre 5 plataformas na tela 
     class novasPlataformas {
         constructor(novoPratoBottom) {
             this.novoPratoBottom = novoPratoBottom;
@@ -67,40 +132,29 @@ function criarPlataforma() {
         };
     };
 
+    // se não tiver nenhuma plataforma em jogo, então crie todas
     if (plataformasEmJogo.length === 0) {
      for (let i = 0; i < quantidadeDePlataformas; i++) {
         let platGap = 600 / quantidadeDePlataformas;
         let novoPratoBottom = 100 + i * platGap;
         const chamarClass = new novasPlataformas(novoPratoBottom);
         }
-    } else if (plataformasEmJogo.length < 5 && plataformasEmJogo.length > 0) {
+    }
+    // mas se tiver faltando crie apenas o que falta
+    else if (plataformasEmJogo.length < 5 && plataformasEmJogo.length > 0) {
         const chamarClass = new novasPlataformas(580);
     }
 
 };
 
+// criar boneco no início do jogo
 function criarBoneco() {
     const boneco = document.createElement("div");
     boneco.classList.add("boneco", "boneco-para-direita");
-    boneco.style.bottom = Number.parseFloat((plataformasEmJogo[0].style.bottom).replace(/[a-zA-Z]/g, '')) + 40 + "px";
+    boneco.style.bottom = Number.parseFloat((plataformasEmJogo[0].style.bottom).replace(/[a-zA-Z]/g, '')) + 60 + "px";
     boneco.style.left = Number.parseFloat((plataformasEmJogo[0].style.left).replace(/[a-zA-Z]/g, '')) + "px";
     grade.appendChild(boneco);
 
-    function cairBoneco() {
-        const intervalo = setInterval(() => {
-            boneco.style.bottom = Number.parseFloat((boneco.style.bottom).replace(/[a-zA-Z]/g, '')) - 5 + "px";
-            verificarElementos();
-        }, 30);
-
-        function verificarElementos() {
-            plataformasEmJogo.forEach(function (elementoAtual, indice) {
-                if (elementoAtual.style.bottom == boneco.style.bottom && elementoAtual.style.left == boneco.style.left) {
-                    clearInterval(intervalo);
-                    moverPlataforma();
-                }
-            })
-        }
-    }
     cairBoneco();
 };
 
